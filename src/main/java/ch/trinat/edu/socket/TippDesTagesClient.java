74663f56c299d7592656b5415d6cca8113214504
domsut <dominiksuter@hotmail.fr>
2020-12-15 18:03:37 +0100

    tipp des tages

--- src/main/java/ch/trinat/edu/socket/TippDesTagesClient.java
+++ src/main/java/ch/trinat/edu/socket/TippDesTagesClient.java
@@ -1,4 +1,29 @@
 package ch.trinat.edu.socket;
 
-public class TippDesTagesClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class   TippDesTagesClient {
+    public void los() {
+        try (Socket s = new Socket("localhost", 4242)) {
+
+            InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+
+            String advice = reader.readLine();
+            System.out.println("Ratschlag fuer heute: " + advice);
+
+            reader.close();
+
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        TippDesTagesClient client = new TippDesTagesClient();
+        client.los();
+    }
 }
--- src/main/java/ch/trinat/edu/socket/TippDesTagesServer.java
+++ src/main/java/ch/trinat/edu/socket/TippDesTagesServer.java
@@ -1,4 +1,38 @@
 package ch.trinat.edu.socket;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TippDesTagesServer {
+    String[] tips = {"Geben Sie Vollgas", "Leben Sie jeden Tag als waere dieser Ihrer letzter", "Unmoeglich! Gibt es nicht", "Seien Sie ehrlich, nur heute mal. Sagen Sie Ihrem Boss, was Sie *wirklich* denken.", "Geben Sie jeden Tag Ihr Bestes"};
+
+    public void los() {
+
+        try (ServerSocket serverSock = new ServerSocket(4242)) {
+
+            while(true) {
+                Socket sock = serverSock.accept();
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                String tipp = getTipp();
+                writer.println(tipp);
+                writer.close();
+                System.out.println(tipp);
+            }
+
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    } // los() schliessen
+
+    private String getTipp() {
+        int random = (int) (Math.random() * tips.length);
+        return tips[random];
+    }
+
+    public static void main(String[] args) {
+        TippDesTagesServer server = new TippDesTagesServer();
+        server.los();
+    }
 }
